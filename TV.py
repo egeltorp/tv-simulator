e4c9e64bb5540c997a9cfb165e2e5321f0db060f
Theodor Holmberg <35803316+egeltorp@users.noreply.github.com>
2025-09-16 12:45:01 +0200

    start TV.py

---
+++ TV.py
@@ -0,0 +1,14 @@
+class TV:
+    def __init__(self, tv_name, current_volume, current_channel, max_volume, max_channel):
+        self.tv_name = tv_name
+        self.current_volume = current_volume
+        self.current_channel = current_channel
+        self.max_volume = max_volume
+        self.max_channel = max_channel
+
+    def change_channel(self, new_channel):
+        if 1 <= new_channel <= self.max_channel:
+            self.current_channel = new_channel
+            print(f"Channel changed to {self.current_channel}")
+        else:
+            print("Invalid channel choice. Try again.")
