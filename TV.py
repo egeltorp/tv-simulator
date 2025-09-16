4a147f278d7dd10acef916c7e2b6b4a96d96a183
pipdog <oscar.ishii@gmail.com>
2025-09-16 13:40:05 +0200

    .

--- TV.py
+++ TV.py
@@ -1,10 +1,10 @@
 class TV:
-    def __init__(self, tv_name, current_volume, current_channel, max_volume, max_channel):
+    def __init__(self, tv_name, max_channel, current_channel, max_volume, current_volume):
         self.tv_name = tv_name
-        self.current_volume = current_volume
+        self.max_channel = max_channel
         self.current_channel = current_channel
         self.max_volume = max_volume
-        self.max_channel = max_channel
+        self.current_volume = current_volume
 
     def change_channel(self, new_channel):
         if 1 <= new_channel <= self.max_channel:
@@ -15,20 +15,41 @@ class TV:
 
     def increase_volume(self):
         if self.current_volume != self.max_volume:
-            current_volume += 1
+            self.current_volume += 1
             return True
         else:
             return False
 
-    def decrease_volume(self)
-        if 0 < self.current_volume < self.max_volume
+    def decrease_volume(self):
+        if 0 < self.current_volume < self.max_volume:
             self.current_volume += 1
             return True
         else:
             return False
         
     def __str__(self):
-        pass
+        return f"{self.tv_name}, channel: {self.current_channel}, volume: {self.current_volume}"
 
     def str_for_file(self):
         pass
+
+def tests():
+        tv = TV("Vardagsrum TV", 100, 22, 10, 9)
+        tv2 = TV("Sovrums TV", 50, 7, 20, 4)
+        print(tv)
+        print(tv2)
+        print(tv.increase_volume())
+        print(tv)
+        print(tv2)
+        print(tv.increase_volume())
+        print(tv)
+        print(tv2)
+        print(tv.change_channel(55))
+        print(tv)
+        print(tv2)
+        print(tv2.change_channel(55))
+        print(tv)
+        print(tv2)
+
+if __name__ == "__main__":
+    tests()
