92e3f7435327ca09db3363ad1a69c09d1fe474c8
pipdog <oscar.ishii@gmail.com>
2025-09-16 14:09:52 +0200

    .

--- TV_simulator.py
+++ TV_simulator.py
@@ -1,4 +1,14 @@
 from TV import TV
 
-def read_file(file):
-    pass
+def read_file(file_path):
+    tv_list = []
+    with open(file_path, 'r') as file:
+        for line in file.readlines():
+            tv_name, *variables  = line.split(',')
+            for v in variables:
+                v = int(v)
+            print(variables)
+    return tv_list
+
+tv_list = read_file("TV.txt")
+
