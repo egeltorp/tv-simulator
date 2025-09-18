cfb19cf9358be30a6658b0f990c9426a9c31e0ea
Theodor Holmberg <35803316+egeltorp@users.noreply.github.com>
2025-09-18 14:54:21 +0200

    add comments

--- TV_simulator.py
+++ TV_simulator.py
@@ -1,5 +1,6 @@
 from TV import TV
 
+# read .txt file and convert strings to list of TV-class objs
 def read_file(file_path):
     tv_list = []
     with open(file_path, 'r') as file:
@@ -10,12 +11,14 @@ def read_file(file_path):
             tv_list.append(tv)
     return tv_list
 
+# write all current TV-class objs to .txt with new/current values
 def write_file(tv_list, file_path):
     with open(file_path, 'w') as file:
         for tv in tv_list:
             s = tv.str_for_file()
             file.write(s)
 
+# menu to choose TV-class methods, returns choice: int
 def adjust_TV_menu(tv: TV):
     quit_menu = False
     while True:
@@ -36,6 +39,7 @@ def adjust_TV_menu(tv: TV):
                 return choice
             print("Ogiltigt. Försök igen!")
 
+# menu to choose which TV-class obj to currently simulate, returns obj
 def select_TV_menu(tv_list: list):
     print("\n--- HUVUDMENYN ---")
     for i, tv in enumerate (tv_list, start=1):
@@ -58,19 +62,24 @@ def select_TV_menu(tv_list: list):
     tv = tv_list[choice - 1]
     return tv
 
+# full program loop
 def simulator():
-    # initializing
+    # initializing and accessing list of TV-class objs
     tv_list = read_file("TV.txt")
     tv = tv_list[1]
+
+    # intro text
     print("\n*** Välkommen till TV-simulatorn ***")
     print()
 
+    # simulation loop
     while True:
         tv = select_TV_menu(tv_list)
         if tv == None:
             break
         while True:
             choice = adjust_TV_menu(tv)
+            # choice 1: change channel
             if choice == 1:
                 while True:
                     new_channel = int(input("Välj ny kanal: "))
@@ -79,15 +88,21 @@ def simulator():
                     else:
                         tv.change_channel(new_channel)
                         break
+            # choice 2: decrease vol, choice 3: increase vol
             elif choice in (2, 3):
                 print(f"Volym: {tv.current_volume}")
                 actions = {2: tv.decrease_volume, 3: tv.increase_volume}
                 actions[choice]()
+            # choice 4: exit TV adjust menu
             elif choice == 4:
                 break
+    
+    # if both loops exited: quit program 
+    # after saving current TV-class obj states
     write_file(tv_list, "TV.txt")
     print("Avslutar.")
 
-
+# run simulator() if program is run in terminal as itself 
+# and NOT imported as a module in other program
 if __name__ == "__main__":
     simulator()
